76cc55f44b91e4762b50e63e409e1e3094e5ad6a
Jenny <chenyixuan09@gmail.com>
2021-03-09 21:00:50 -0500

    add readme and resources folder

--- main.py
+++ main.py
@@ -3,6 +3,6 @@
 # Press ⇧F10 to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+# I add some comments here so you might see conflicts.
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
