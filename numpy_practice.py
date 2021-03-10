444389924551d52397e995f0440ad914e720d123
Jenny <chenyixuan09@gmail.com>
2021-03-09 20:40:34 -0500

    add numpy pandas homework

---
+++ numpy_practice.py
@@ -0,0 +1,23 @@
+# numpy, pandas, matplotlib practice
+
+# Watch those materials if possible
+
+# numpy tutorial: https://www.w3schools.com/python/numpy_intro.asp
+
+# pandas tutorial: https://www.youtube.com/watch?v=PfVxFV1ZPnk
+
+# funtional programming youtube video: https://www.youtube.com/watch?v=goypZR_lQ7I
+
+# matplotlib tutorial: https://matplotlib.org/tutorials/index.html
+
+# practice:
+
+# 首先熟悉resources 下面是不同的人对于不同APP内容的评分，分别存储在几个不同的文件里。每个文件头上是各个栏的TITLE。
+# 其次通过数据处理，进行数据的合并，生成一个EXCEL表格，将用户评分的如下信息列出来。注意有的客户可能对几个不同的内容打分，并且有可能对同一个内容进行多次评分。
+# EXCEL表格内容包括  用户基本信息
+# 通过图标进行一些简单的数据分析，例如，什么从用户角度，什么职业的人最多，用户评分最多是什么类别，再比如，从内容角度，哪种类目最受欢迎，等等* (try yourself)
+
+
+
+
+
